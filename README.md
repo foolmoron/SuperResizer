64e62755f4a12bc87918654ec0bed8bcd5fa6749
Momin Khan <foolmoron@gmail.com>
2014-12-06 12:05:50 -0500

    Basic file structure setup

---
+++ README.md
@@ -0,0 +1,4 @@
+SuperResizer
+============
+
+Ludum Dare 31 entry. Play the game by resizing and moving the browser window!
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// global object for stuff
+window.SR = window.SR || {};
+
+(function() {
+  console.log("Hey sup");
+})();
